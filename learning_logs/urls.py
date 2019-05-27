c51efefa23c128285c8f7aa640b5d36794d58cf3
Rodrigo <rodrigobdias@gmail.com>
2019-05-27 16:01:02 -0300

    Criando Páginas

---
+++ learning_logs/urls.py
@@ -0,0 +1,16 @@
+"""Define padrões de URL para learning_logs."""
+
+from django.urls import path, re_path
+
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    # Página inicial
+    path('', views.index, name='index'),
+    # Mostra todos os assuntos
+    path(r'topics/', views.topics, name='topics'),
+    # Página de detalhes para um único assunto
+    re_path(r'^topics/(?P<topic_id>\d+)/', views.topic, name='topic'),
+]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Topic
+
+def index(request):
+    """A página inicial de Learning Log"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Mostra um único assunto etodas as suas entradas."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+ 
